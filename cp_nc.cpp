5698a7b2da4cd34ed39beff8a78a7135a574af06
Niladri Chakraborty <104026598+im-nc@users.noreply.github.com>
2024-05-20 14:13:09 +0530

    Update cp_nc.cpp

--- cp_nc.cpp
+++ cp_nc.cpp
@@ -1,8 +1,13 @@
 /*
 
-✎ nc ... (date)
+✎ nc ... (DD.MM.YYYY)
 
 */
+
+#ifndef LOCAL
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#endif
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -29,12 +34,74 @@ using pii = pair<int, int>;
 template <typename T>
 using vc = vector<T>;
 
+#define vi vc<int>
 #define maxar(arr) *max_element(all(arr))
 #define minar(arr) *min_element(all(arr))
 
+unsigned long long factorial(int n) {
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; ++i) {
+        fact *= i;
+    }
+    return fact;
+}
+
+unsigned long long nCr(int n, int r) {
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+unsigned long long nPr(int n, int r) {
+    return factorial(n) / factorial(n - r);
+}
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+unsigned long long lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isPalindrome(int n) {
+    int original = n;
+    int reversed = 0;
+    while (n > 0) {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return original == reversed;
+}
+
+int find_mex(const std::vector<int>& vec) {
+    std::unordered_set<int> unique_elements(vec.begin(), vec.end());
+    int mex = 0;
+    while (unique_elements.find(mex) != unique_elements.end()) {
+        mex++;
+    }
+    return mex;
+}
+
 const int MAX = 1e5 + 5;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
 const ld EPS = 1e-9;
 
 #define arrin(arr, n)              \
@@ -57,7 +124,7 @@ using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node
 
 void sol()
 {
-    // code begins here.
+    //code here
 }
 
 signed main()
